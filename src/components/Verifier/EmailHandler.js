1f85a8459997911fd3b2c0c3f8751a08b8a39bad
Samim MBT <samimmbt@gmail.com>
2024-02-22 18:07:54 +0330

    Not much Change

--- src/components/Verifier/EmailHandler.js
+++ src/components/Verifier/EmailHandler.js
@@ -1,12 +1,16 @@
-// import {gnerateCode} from './code'
+import gnerateCode from './code'
 import emailjs from '@emailjs/browser';
-require('dotenv').config()
+// require('dotenv').config()
+// console.log(process.env)
+export default function sendTo(mail, code) {
+    emailjs.init({
+        publicKey: "s9e-8A8ltIc21Fbtc",
+    })
 
-console.log(process.env)
-export default function sendTo (mail, code) {
     emailjs.send("service_tgx4l9o", "template_f8rwu2g", {
+        publicKey: 's9e-8A8ltIc21Fbtc',
         to_name: mail,
-        message: code,
+        message: gnerateCode()
     }).then(
         () => {
             console.log('SUCCESS!');
