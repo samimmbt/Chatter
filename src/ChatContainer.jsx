8e47f29bb241be14fd735de4af1c33a210737680
Samim MBT <samimmbt@gmail.com>
2024-02-28 08:14:32 +0330

    add container for chat and its logic

---
+++ src/ChatContainer.jsx
@@ -0,0 +1,11 @@
+import Chat from "./components/Chat/Chat";
+import ChatList from "./components/ChatList/ChatList";
+
+export default function ChatContainer() {
+    return(
+        <>
+        <ChatList/>
+        <Chat/>
+        </>
+    )
+}
