932c55dd15e6871faf035af02471475abcd2733c
Samim MBT <samimmbt@gmail.com>
2024-03-01 21:40:33 +0330

    Update login logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link,useNavigate } from "react-router-dom"
 import Button from "./Button/Button"
 import Verfier from "./Verifier/Verifier"
-import { useState } from "react"
+import { useState,useEffect } from "react"
+import socketIO from 'socket.io-client';
+import sendTo from './Verifier/EmailHandler'
+// const socket = socketIO.connect('http://localhost:3000');
 const Login = () => {
     const [emailin, setemailin] = useState()
-    const [disabled, setDisabled] = useState()
+    const [disabled, setDisabled] = useState(true)
     const [codeSent, setCodeSent] = useState(false)
+    const usr = localStorage.getItem('usr');
+    const userObj = JSON.parse(usr);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!userObj || !userObj.logged) {
+            console.log('Not logged in');
+            navigate('/'); // Redirect to login page
+        } else {
+            console.log('User is logged in');
+            navigate('/app');
+        }
+    }, []);
     const sendCode = () => {
         setCodeSent(true)
+        sendTo().then().catch();
     }
     const timer = (state) => {
         setDisabled(state)
+        console.log(state);
     }
     const email = (value) => {
         setemailin(value)
@@ -20,14 +38,17 @@ const Login = () => {
         h1: {
             textAlign: "center",
             fontSize: "6em"
+        },
+        h6: {
+            marginLeft: "8%"
         }
     }
     return (
         <div className="parent_parent">
             <h1 style={style.h1}>CHATTER</h1>
-            <Verfier id="email" getVerifedValue={email} timeout={timer}/>
-            <Button id="login" value='Login' click={sendCode} disable={disabled} />
-            <h6 id="h6-login-signup">No account yet? <div className="link"><Link to={'../signup'}>Sing up</Link></div></h6>
+            <Verfier id="email" getVerifedValue={email} timeout={timer} />
+            {emailin && <Button id="login" value='Login' click={sendCode} disable={disabled} />}
+            <h6 id="h6-login-signup" style={style.h6}>No account yet? <div className="link"><Link to={'signup'}>Sing up</Link></div></h6>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,38 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import "./index.css";
 import ErrorPage from './components/Error/ErrorPage.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
+import ChatContainer from './ChatContainer.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/app",
+    element: <App />,
+    errorElement: <ErrorPage />,
     children:
       [
         {
-          path: "app",
-          element: <App/>,
-          children: [
-            {
-              path: "../signup",
-              element: <Signup />
-            },
-          ]
-        },
+          index:true,
+          element: <ChatContainer />,
+        }
       ]
   }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  ///* </React.StrictMode> */
 )
