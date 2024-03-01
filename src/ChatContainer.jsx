8cff45f34ab3ce1be3d649f31e6c5b3c44b7013e
Samim MBT <samimmbt@gmail.com>
2024-03-01 21:42:02 +0330

    Fake data testing , step!

--- src/ChatContainer.jsx
+++ src/ChatContainer.jsx
@@ -1,11 +1,24 @@
 import Chat from "./components/Chat/Chat";
+import ChatFooter from "./components/Chat/ChatFooter";
 import ChatList from "./components/ChatList/ChatList";
 
 export default function ChatContainer() {
+    const userListArray = [
+        {
+            name:"Saved Message",
+            userid:"me",
+            id:0
+        },
+        {
+            name:"Ali",
+            userid:"ali",
+            id:1
+        }
+    ]
     return(
-        <>
-        <ChatList/>
+        <div className="chatContainerBox">
+        <ChatList list={userListArray}/>  
         <Chat/>
-        </>
+        </div>
     )
 }
