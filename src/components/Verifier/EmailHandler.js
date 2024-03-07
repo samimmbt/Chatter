4a2f9e4e75d30bbee11060417ba959efc422730f
Samim MBT <samimmbt@gmail.com>
2024-03-07 20:11:09 +0330

    Getting rid of axios in some code

--- src/components/Verifier/EmailHandler.js
+++ src/components/Verifier/EmailHandler.js
@@ -1,20 +1,24 @@
 import emailjs from '@emailjs/browser';
-import {getEmailData} from '../Connection/DataHandler'
+import { getEmailData } from '../Connection/DataHandler'
 export default async function sendTo(mail, code) {
-   try{
-    /*if(mail!== null && code !== null){
-    const emailData = await getEmailData('ajdy236dvasu623bjs721028456zxjb9');
-    emailjs.send(emailData.service,emailData.template , {
-        publicKey:emailData.pub_key,
-        to_name: mail,
-        message: code
-    }).then(() => {
-            console.log('SUCCESS!');
-        }).catch((error) => {
-            console.log('FAILED...', error.text);
-        })
-    }*/
-} catch (error) {
-    console.error('Error sending email:', error);
-  }
+    try {
+        if (mail !== null && code !== null) {
+            const emailData = await getEmailData('ajdy236dvasu623bjs721028456zxjb9');
+            emailjs.init({
+                publicKey: emailData.pub_key
+            })
+            console.log(emailData);
+            emailjs.send(emailData.service, emailData.template, {
+                publicKey: emailData.pub_key,
+                to_name: mail,
+                message: code
+            }).then(() => {
+                console.log('SUCCESS!');
+            }).catch((error) => {
+                console.log('FAILED...', error.text);
+            })
+        }
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
 }
