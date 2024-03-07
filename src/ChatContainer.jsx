3c37346dbad6d2a42d62f03a670752b2460daa6f
Samim MBT <samimmbt@gmail.com>
2024-03-07 20:19:15 +0330

    killing the local sample data

--- src/ChatContainer.jsx
+++ src/ChatContainer.jsx
@@ -1,58 +1,77 @@
+import Alert from "./components/Bubble/Alert";
 import Chat from "./components/Chat/Chat";
-import ChatFooter from "./components/Chat/ChatFooter";
 import ChatList from "./components/ChatList/ChatList";
-import { useEffect, useState } from "react";
+import Header from "./components/Header/Header";
+import Menu from "./components/Menu/Menu";
+import { currentTime } from "./components/Timer/Timer";
+import { useEffect, useState, useRef } from "react";
 export default function ChatContainer({ socket }) {
     const [messages, setMessages] = useState([]);
     const [userlist, setUserList] = useState([]);
-    const [selected,setSelected] = useState();
-    // useEffect(() => {
-    //     // Set up the socket event listener for 'messageResponse'
-    //     // socket.on('messageResponse', (data) => setMessages([...messages, data]));
+    const [selected, setSelected] = useState();
+    const [isOpen, setIsOpen] = useState(false);
+    const [alertMessage, setAlertMessage] = useState(null);
+    const usr = localStorage.getItem('usr');
+    const userObj = JSON.parse(usr);
+    const lastMessageRef = useRef(null);
 
-    //     // Initialize user list and messages
-    //     const msg = [
-    //         { "from": { userid: "1227uid", name: "samim" }, "time": "12:12:12.2", "seen": true, "msg": "HI" },
-    //         { "from": { userid: "ali123", name: "ali" }, "time": "12:15:19.1", "seen": false, "msg": "hello" },
-    //         { "from": { userid: "ali123", name: "ali" }, "time": "12:19:1.9", "seen": false, "msg": "dssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss" }
-    //     ]
-    //     setMessages(msg);
-        // setUserList(prevUserList => [...prevUserList, ...userListArray]);
-    // }, [socket]);
-    const userListArray = [
-        {
-            name: "Saved Message",
-            userid: "1227uid",
-            id: 0
-        },
-        {
-            name: "ali",
-            userid: "ali123",
-            id: 1
-        }
-    ];
+    useEffect(() => {
+        // Set up the socket event listener for 'messageResponse'
+        socket.on('messageResponse', (data) => {
+            // if (condition) {
 
-    const msg = {"ali":[
-        { "from": { userid: "1227uid", name: "samim" }, "time": "12:12:12.2", "seen": true, "msg": "HI" },
-        { "from": { userid: "ali123", name: "ali" }, "time": "12:15:19.1", "seen": false, "msg": "hello" },
-        { "from": { userid: "ali123", name: "ali" }, "time": "12:19:1.9", "seen": false, "msg": "dssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss" },
-        { "from": { userid: "1227uid", name: "samim" }, "time": "12:12:12.2", "seen": true, "msg": "HI" },
-        { "from": { userid: "ali123", name: "ali" }, "time": "12:19:1.9", "seen": false, "msg": "????????????" },   
-        { "from": { userid: "1227uid", name: "samim" }, "time": "12:12:12.2", "seen": true, "msg": "HI" }
-    ],
-    "Saved Message":[
-        { "from": { userid: "1227uid", name: "samim" }, "time": "12:12:12.2", "seen": true, "msg": "HI" }
-    ]
-}
-    const setSelectedChat = (selected)=>{
+            // }
+            console.log(data);
+            setMessages([...messages, data])
+            lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+        });
+        socket.on('userlist',(users)=>{
+            setUserList([...userlist, userlist])
+        })
+    }, [socket, messages, userlist]);
+
+    const setSelectedChat = (selected) => {
         setSelected(selected)
         console.log(selected);
     }
+    const handelMessage = (message) => {
+        const timearr = currentTime()
+        const time = `${timearr.hour.toString().padStart(2, '0')}:${timearr.minute.toString().padStart(2, '0')}`
+        if (message.trim() && userObj.logged && selected) {
+            socket.emit('message', {
+                "from": {
+                    userid: userObj.userid,
+                    name: userObj.name
+                },
+                "to": {
+                    name: selected
+                },
+                "time": time,
+                "seen": false,
+                "msg": message
+            });
+        } else {
+            setAlertMessage("Select a chat");
 
+        }
+    }
+    const removeError = (erorr) => {
+        if (erorr) {
+            setAlertMessage(null)
+        }
+    }
+    const openMenu = () => {
+        setIsOpen(isOpen ? false : true)
+    }, clicked = (e) => {
+        console.log(e);
+    }
     return (
         <div className="chatContainerBox">
-            <ChatList list={userListArray} chatBox={setSelectedChat}/>
-            <Chat socket={socket} messages={msg[`${selected}`]}/>
+            {isOpen && <Menu menuEvent={openMenu} itemEvent={clicked} />}
+            <Header menuEvent={openMenu} />
+            <ChatList list={userlist} chatBox={setSelectedChat} />
+            <Chat messages={messages} msg={handelMessage} selected={selected} lastMessageRef={lastMessageRef} />
+            {alertMessage && <Alert title='Error' message={alertMessage} duration={10} timeout={removeError} />}
         </div>
     )
 }
